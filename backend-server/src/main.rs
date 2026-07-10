725ebdfb00d025d6ea70c74158fcdf997d4231cb
beginner2026 <beginner2026@proton.me>
2026-07-10 09:25:01 +0200

    init 2

--- backend-server/src/main.rs
+++ backend-server/src/main.rs
@@ -1,4 +1,25 @@
 #[macro_use] extern crate rocket;
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers",
+            kind: Kind::Response
+        }
+    }
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*")); // Erlaubt Zugriff von überall
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+    }
+}
+
 
 #[get("/")]
 fn index() -> &'static str {
@@ -7,5 +28,7 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .attach(CORS)
+        .mount("/", routes![index])
 }
