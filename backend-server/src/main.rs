5c83543c9d808194dd0d5bfaa0f14d135de5861e
beginner2026 <beginner2026@proton.me>
2026-07-11 17:49:38 +0200

    versuch daten zuerhalten

--- backend-server/src/main.rs
+++ backend-server/src/main.rs
@@ -1,19 +1,13 @@
-use rocket::http::Header;
-use rocket::{Request, Response};
+use rocket::http::{Header,Method} ;
+use rocket::{Request, Response, Data};
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::{get, post, routes, State};
+use rocket::{get, post, routes, State, options};
 use surrealdb::engine::local::Db;
 use surrealdb::Surreal;
 use serde::{Serialize, Deserialize};
 use rocket::serde::json::Json;
-// Deine Datenstruktur. 
-// Wichtig in SurrealDB 3.x+: Eigene Structs müssen zusätzlich `SurrealValue` implementieren!
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct LoginRequest {
-    login_username: String,
-    login_password: String,
-}
+
+
 // Wir definieren ein Struct für die Antwort an Tauri
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -25,26 +19,43 @@ struct LoginResponse {
 // Typ-Alias für saubereren Code in den Routen
 type DbState = Surreal<Db>;
 pub struct CORS;
-
 #[rocket::async_trait]
 impl Fairing for CORS {
     fn info(&self) -> Info {
         Info {
             name: "Add CORS headers",
-            kind: Kind::Response
+            kind: Kind::Request 
         }
     }
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*")); // Erlaubt Zugriff von überall
+
+async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
         response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, OPTIONS"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+
+        // Wenn es OPTIONS war, zwingend mit 200 OK antworten
+        if request.method() == Method::Options {
+            response.set_status(rocket::http::Status::Ok);
+            response.set_sized_body(0, std::io::Cursor::new(""));
+        }
     }
 }
-
+#[options("/<path..>")]
+fn all_options(path: std::path::PathBuf) -> &'static str {
+    ""
+}
 
 #[get("/")]
 async fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct LoginRequest {
+    login_username: String,
+    login_password: String,
 }
 
 #[post("/login", data = "<login_data>")]
@@ -52,6 +63,11 @@ async fn login(
     db: &State<DbState>,
     login_data: Json<LoginRequest>
     ) -> Result<Json<LoginResponse>, String> {
+
+    let username = &login_data.login_username;
+    let paswort = &login_data.login_password;
+    rocket::info!("Name {}, Passwort {}", username, paswort);
+
     Ok(Json(LoginResponse {
         message: "Login successful!".into(),
         success: true,
@@ -78,7 +94,9 @@ async fn main() -> Result<(), rocket::Error> {
 
     // 3. Rocket starten und die DB als State übergeben
     let _ = rocket::build()
+        .attach(CORS)
         .manage(db) // Hier wird die DB für alle Routen registriert
+        .mount("/", routes![all_options]) // <-- Hier mitsenden!
         .mount("/api/auth", routes![login, register])
         .launch()
         .await?;
